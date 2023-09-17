76c61a7a35ce0f3fa81c5d7e1a07577c5e0dc675
XitaRps <xitarps@gmail.com>
2023-09-17 18:00:45 -0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
   
   resources :articles
 
-  resources :users
+  # resources :users
+
+  get 'singnup', to: 'users#new'
+  get 'singnup', to: 'users#create'
+  get 'singnup', to: 'users#update'
+
+  get 'signin', to: 'sessions#new'
+  post 'signin', to: 'sessions#create'
+  delete 'signout', to: 'sessions#destroy'
 end
